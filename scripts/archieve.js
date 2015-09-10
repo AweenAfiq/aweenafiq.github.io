dab2f2980414a4e15d13dff6aacbfbdcce3df5d5
Afiq Xilantra Azmi <artxlave@gmail.com>
2015-09-10 08:43:59 +0800

    Yeay!
    Without Neon. Because Neon is damn hard..for now.

---
+++ scripts/archieve.js
@@ -0,0 +1,39 @@
+      var elem = document.querySelector('#mainToolbar');
+  var player = elem.animate([
+    {opacity: "0", transform: "translateY(-50px)"},
+    {opacity: "1.0", transform: "translateY(0px)"}
+  ], {
+    direction: "alternate", duration: 600, easing: 'ease-in-out'
+  });
+    
+    var elem = document.querySelector('.satu');
+  var player = elem.animate([
+    {opacity: "0", transform: "translateY(50px)"},
+    {opacity: "1.0", transform: "translateY(0px)"}
+  ], {
+    direction: "alternate", duration: 500, easing: 'ease-in-out'
+  });
+    
+var elem = document.querySelector('.dua');
+  var player = elem.animate([
+    {opacity: "0", transform: "translateY(50px)"},
+    {opacity: "1.0", transform: "translateY(0px)"}
+  ], {
+    direction: "alternate", duration: 600, easing: 'ease-in-out'
+  });
+     
+var elem = document.querySelector('.tiga');
+  var player = elem.animate([
+    {opacity: "0", transform: "translateY(50px)"},
+    {opacity: "1.0", transform: "translateY(0px)"}
+  ], {
+    direction: "alternate", duration: 700, easing: 'ease-in-out'
+  });
+    
+var elem = document.querySelector('.garis');
+  var player = elem.animate([
+    {opacity: "0", transform: "scale(0)"},
+    {opacity: "1.0", transform: "scale(1)"}
+  ], {
+    direction: "alternate", duration: 1951, easing: 'ease-in-out'
+  });
